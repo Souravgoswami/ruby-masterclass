f7356708a526edbedd3ba7b45b416900fef4a0fc
Sourav Goswami <37468605+Souravgoswami@users.noreply.github.com>
2019-06-02 03:18:21 +0530

    Update anagrams.rb

--- Part 38: Anagrams/anagrams.rb
+++ Part 38: Anagrams/anagrams.rb
@@ -3,12 +3,13 @@
 WORDFILE = File.join(File.dirname(__FILE__), 'words')
 
 unless File.exist?(WORDFILE)
-	puts "#{WORDFILE} doesn't exist."
-	exit! 0
+	puts "#{WORDFILE} doesn't exist. Please get it from here: "\
+		'https://github.com/Souravgoswami/ruby-masterclass/blob/master/Part%2038:%20Anagrams/words'
+	exit 0
 end
 
-WORDS = IO.readlines(WORDFILE).map { |w| w.strip.downcase }[1..-1].uniq.select { |x| x.scan(/[^a-z]/).empty? }
-SORTED_WORDS = WORDS.map { |w| w.chars.sort.join }
+WORDS = IO.readlines(WORDFILE).map { |w| w.strip.downcase }.uniq.select { |x| x !~ /[^a-z]/ }
+SORTED_WORDS = WORDS.map { |w| w.unpack('c*').sort.pack('c*') }	# This is significantly faster than w.chars.sort.join
 WORDS_SIZE = WORDS.size
 
 def search(word = WORDS.sample)

5c2deb28ccbf409047267663cc76251645026f13
Sourav Goswami <souravgoswami@protonmail.com>
2019-06-20 01:35:36 +0530

    Delete Part11_Temperature_Converter.rb

--- Section 5: Basic Arithmetic I/Part11_Temperature_Converter.rb
+++
@@ -1,4 +0,0 @@
-# PART 11                                                         
-# The formula is (T - 32) * (5 / 9) or (T - 32 * 0.555555)        
-p (STDIN.gets.to_i - 32 * (5 / 9.0)).round(2)                     # => 82.22
-p STDIN.gets.to_i.-(32).*(5 / 9.0).round(2)                       # => 37.78

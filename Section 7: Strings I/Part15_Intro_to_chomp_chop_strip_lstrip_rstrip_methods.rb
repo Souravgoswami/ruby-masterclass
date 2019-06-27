f07d3331b33f238a95137fc3e15080597ca16f87
Sourav Goswami <souravgoswami@protonmail.com>
2019-06-27 14:17:49 +0530

    Delete Part15_Intro_to_chomp_chop_strip_lstrip_rstrip_methods.rb

--- Section 7: Strings I/Part15_Intro_to_chomp_chop_strip_lstrip_rstrip_methods.rb
+++
@@ -1,32 +0,0 @@
-#!/usr/bin/ruby -w                                                             
-# PART 15                                                                      
-# The chomp method, we used earlier, is used to strip off the extra newline    
-# character at the end of a string:                                            
-p "a\n\n\n".chomp                                                              # => "a\n\n"
-p "Ruby!".chomp                                                                # => "Ruby!"
-                                                                               
-# chop method chops out the last character:                                    
-p "a\n\n\n".chomp                                                              # => "a\n\n"
-p "Ruby!".chop                                                                 # => "Ruby"
-                                                                               
-# The strip method removes trailing white space, new line,                     
-# tab, form feed, carriage return characters from both                         
-# right and left side of the string                                            
-p " \n\t\fRuby!\n\t\f\r ".strip                                                # => "Ruby!"
-p "Ruby!".strip                                                                # => "Ruby!"
-                                                                               
-# The lstrip method only strips the trailing left whitespace:                  
-p " \n\t\fRuby!\n\t\f\r ".lstrip                                               # => "Ruby!\n\t\f\r "
-                                                                               
-# The rstrip method only strips the trailing right whitespace                  
-p " \n\t\fRuby!\n\t\f\r ".rstrip                                               # => " \n\t\fRuby!"
-                                                                               
-# They all have bang methods!                                                  
-a =  " \n\t\fRuby!\n\t\f\r "                                                   # =>  
-	Ruby!
-	 
-p a.lstrip!                                                                    # => "Ruby!\n\t\f\r "
-p a.rstrip!                                                                    # => "Ruby!"
-p a.strip!                                                                     # => nil
-p a.chomp!                                                                     # => nil
-p a.chop!                                                                      # => "Ruby"

ac1b8840f7853fd17d59c91f1641605c3eba6908
Sourav Goswami <souravgoswami@protonmail.com>
2019-06-27 14:19:56 +0530

    Delete Part19_Intro_to_squeeze_squeeze!_methods.rb

--- Section 7: Strings I/Part19_Intro_to_squeeze_squeeze!_methods.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby -w                                                 
-# PART 19                                                          
-# The include method can check if a stirng contains a substring    
-p 'Hello'.include?('e')                                            # => true
-p 'This is Ruby'.include?('Ruby')                                  # => true
-                                                                   
-a = 'Once upon a time, in a galaxy far far away'                   # => Once upon a time, in a galaxy far far away
-p a.include? 'Once'                                                # => true
-p a.include? 'once'                                                # => false

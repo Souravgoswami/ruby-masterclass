d08e980db7494849c2baf0badbc10b4c18705a20
Sourav Goswami <souravgoswami@protonmail.com>
2019-06-27 14:17:42 +0530

    Delete Part16_Intro_to_index_rindex_methods.rb

--- Section 7: Strings I/Part16_Intro_to_index_rindex_methods.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/ruby -w                                                
-# PART 16                                                         
-# The index method returns the index of a character from left:    
-p 'Ruby is my favourite programming language!'.index('u')         # => 1
-# Here it's 1 because it's after R, which is in index 0           
-                                                                  
-# The rindex counts from right                                    
-p 'Ruby is my favourite programming language!'.rindex('u')        # => 37
-# This counts the index of language's u, which is at 37           

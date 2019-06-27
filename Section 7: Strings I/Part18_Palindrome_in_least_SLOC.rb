b3652f72cdb5006cf572694500baea0cae88a0a4
Sourav Goswami <souravgoswami@protonmail.com>
2019-06-27 14:20:06 +0530

    Delete Part18_Palindrome_in_least_SLOC.rb

--- Section 7: Strings I/Part18_Palindrome_in_least_SLOC.rb
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/ruby -w
-print('Enter a word: ') || puts(STDIN.gets.to_s.strip.downcase.then { |x| x.empty? ? '' : (x.concat(x.reverse == x ? ' is' : ' doesn\'t look like')) + ' a Palindrome' })

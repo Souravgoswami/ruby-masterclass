51af9b52bd9c9a598bd6cb39c5bf22ed56447ba5
Sourav Goswami <souravgoswami@protonmail.com>
2019-06-29 20:36:54 +0530

    Add files via upload

--- Section 9: Conditions/Part10_the_case_statement.rb
+++ Section 9: Conditions/Part10_the_case_statement.rb
@@ -8,11 +8,12 @@ print 'Enter your age: '
 
 # Case uses the case subsumption operator. We had a brief intro to it previoiusly
 puts(
-	'You are a ' + case STDIN.gets.to_i
+	'You are ' + case STDIN.gets.to_i
 		when 0...3 then 'baby'
 		when 3..16 then 'children'
 		when 17..30 then 'young adult'
 		when 31..45 then 'middle aged adults'
-		else 'old adult'
-	end.split.map(&:capitalize).join(' ')
+		when 45..60 then 'old adult'
+		else 'senior citizen'
+	end.split.map(&:capitalize).join(' ').then { |x| (x[0] =~ /[AEIOU]/ ? 'an ' : 'a ') + x }
 )

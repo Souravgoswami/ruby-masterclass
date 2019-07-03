1873ac0c916d85cff903b88ca29241f9d14ae13d
Sourav Goswami <souravgoswami@protonmail.com>
2019-07-03 18:01:59 +0530

    Delete Part10_Quiz_Converting_range_of_hexadecimal_to_decimal.rb

--- Section 12: Range Objects/Part10_Quiz_Converting_range_of_hexadecimal_to_decimal.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/ruby -w
-# PART 10
-
-# Quiz: Converting range of hexadecimal to decimal
-p (0x00..0xF).to_a                                    # => [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15]
